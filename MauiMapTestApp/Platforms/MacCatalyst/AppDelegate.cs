5c5c9d8c2fea38d4c1b9f468533b7f2ead0ed035
Yazkard <jakubprzygodzki@gmail.com>
2022-09-11 20:23:53 +0200

    Deleted unused platform files

--- MauiMapTestApp/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiMapTestApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
