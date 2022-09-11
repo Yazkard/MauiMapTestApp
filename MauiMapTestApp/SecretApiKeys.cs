007c79b5898fdbcdbf69550e45e181226c774f84
Yazkard <jakubprzygodzki@gmail.com>
2022-09-11 23:32:04 +0200

    added api keys file

---
+++ MauiMapTestApp/SecretApiKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiMapTestApp
+{
+    public static class SecretApiKeys
+    {
+        public const string ArcGisApiKey = "YOUR_API_KEY";
+    }
+}
