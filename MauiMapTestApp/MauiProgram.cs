c88402491b7cded0feee5c6d3a2f4ddf84e21857
Yazkard <jakubprzygodzki@gmail.com>
2022-09-20 01:58:42 +0200

    added MainPageView with map from ArcGIS

--- MauiMapTestApp/MauiProgram.cs
+++ MauiMapTestApp/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiMapTestApp;
+﻿using Esri.ArcGISRuntime.Maui;
+
+namespace MauiMapTestApp;
 
 public static class MauiProgram
 {
@@ -11,8 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			}).UseArcGISRuntime();
 
-		return builder.Build();
+        return builder.Build();
 	}
 }
---
+++ MauiMapTestApp/Source/Presentation/MainPage/MainPageView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using ReactiveUI.Maui;
+
+namespace MauiMapTestApp.Source.Presentation.MainPage;
+
+public partial class MainPageView : ReactiveContentPage<MainPageViewModel>
+{
+	public MainPageView()
+	{
+        ViewModel = new MainPageViewModel();
+        InitializeComponent();
+        mapView.Map = new Esri.ArcGISRuntime.Mapping.Map(new Basemap(BasemapStyle.ArcGISTerrainDetail)
+        {
+            ApiKey = SecretApiKeys.ArcGisApiKey
+        });
+    }
+}
+
